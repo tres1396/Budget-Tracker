c98f2be89c92da51cf0df287e336cf9b543ba000
Tre Scott <tres1396@gmail.com>
2021-05-31 18:36:30 -0400

    finishing database.js

--- public/database.js
+++ public/database.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let db;
 let budgetVersion;
 
@@ -17,3 +19,63 @@ request.onupgradeneeded = function (e) {
     db.createObjectStore("BudgetStore", { autoIncrement: true });
   }
 };
+
+request.onerror = function (e) {
+  console.log(`Error ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+  console.log("Checking database");
+
+  let transaction = db.transaction([BudgetStore], "readwrite");
+
+  const store = transaction.objectStore("BudgetStore");
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(["BudgetStore"], "readwrite");
+
+            const currentStore = transaction.objectStore("BudgetStore");
+
+            currentStore.clear();
+            console.log("Store cleared");
+          }
+        });
+    }
+  };
+}
+
+request.onsuccess = function (e) {
+  console.log("success");
+  db = e.target.result;
+
+  if (navigator.onLine) {
+    console.log("Backend is online");
+    checkDatabase();
+  }
+};
+
+const saveRecord = (record) => {
+  console.log("Save record invoked");
+
+  const transaction = db.transaction(["BudgetStore"], "readwrite");
+
+  const store = transaction.objectStore("BudgetStore");
+
+  store.add(record);
+};
+
+window.addEventListener("online", checkDatabase);
